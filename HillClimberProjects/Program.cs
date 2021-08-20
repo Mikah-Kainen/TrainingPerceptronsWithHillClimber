fcaf3110049d2738677b89fedea336eb1a9fcf8c
Mikah Kainen <mikah@mltd.com>
2021-08-20 14:04:40 -0700

    Did some super cool stuff pretty sure it works but its kinda hard to tell

--- HillClimberProjects/Program.cs
+++ HillClimberProjects/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace HillClimberProjects
 {
@@ -9,7 +10,7 @@ namespace HillClimberProjects
         {
             Random random = new Random();
             string returnString = "";
-            for(int i = 0; i < length; i ++)
+            for (int i = 0; i < length; i++)
             {
                 returnString += (char)random.Next(33, 127);
             }
@@ -21,19 +22,19 @@ namespace HillClimberProjects
             Random random = new Random();
             int mutateIndex = random.Next(0, current.Length);
             int changeValue = random.Next(0, 2);
-            if(current[mutateIndex] == 126)
+            if (current[mutateIndex] == 126)
             {
                 changeValue = -1;
             }
-            else if(current[mutateIndex] == 33)
+            else if (current[mutateIndex] == 33)
             {
                 changeValue = 1;
             }
-            else if(changeValue == 0)
+            else if (changeValue == 0)
             {
                 changeValue = -1;
             }
-           
+
 
             string returnString = current;
             char[] returnValue = current.ToCharArray();
@@ -44,35 +45,96 @@ namespace HillClimberProjects
         static float CalculateError(string target, string current)
         {
             float numerator = 0;
-            for(int i = 0; i < target.Length; i ++)
+            for (int i = 0; i < target.Length; i++)
             {
                 numerator += Math.Abs(target[i] - current[i]);
             }
             return numerator / target.Length;
         }
 
+        static float CalculateError(Point[] points, (float, float) currentLine)
+        {
+            float currentError = 0;
+            foreach (Point point in points)
+            {
+                currentError += Math.Abs(point.Y - currentLine.Item1 * point.X - currentLine.Item2);
+            }
+            return currentError;
+        }
+
+        static (float, float) MutateLine(Point[] points, (float, float) currentLine)
+        {
+            float currentError = CalculateError(points, currentLine);
+            Random random = new Random();
+            int whatToChange;
+            (float, float) nextLine;
+            do
+            {
+                whatToChange = random.Next() % 2;
+                nextLine = currentLine;
+                if (whatToChange == 0)
+                {
+                    nextLine.Item1 += random.Next(1, 201) / 100f - 1;
+                }
+                else
+                {
+                    nextLine.Item2 += random.Next(1, 201) / 100f - 1;
+                }
+            }
+            while (currentError < CalculateError(points, nextLine));
+            return nextLine;
+        }
+
+        static (float, float) FindBestLine(Point[] points)
+        {
+            (float, float) line = (0, 0);
+            for (int i = 0; i < 1000; i++)
+            {
+                line = MutateLine(points, line);
+            }
+            return line;
+        }
+
         static void Main(string[] args)
         {
-            string target = "Hello World!";
-            string start = GenerateString(target.Length);
-            float currentError = CalculateError(target, start);
-            Console.WriteLine("StartVl: " + start + " Error: " + currentError);
-            Stopwatch Delay = new Stopwatch();
-            Stopwatch.StartNew();
+            //string target = "Hello World!";
+            //string start = GenerateString(target.Length);
+            //float currentError = CalculateError(target, start);
+            //Console.WriteLine("StartVl: " + start + " Error: " + currentError);
+            //Stopwatch Delay = new Stopwatch();
+            //Stopwatch.StartNew();
 
-            while(currentError != 0)
+            //while(currentError != 0)
+            //{
+            //    string temp = MutateString(start);
+            //    if(currentError > CalculateError(target, temp))
+            //    {
+            //        start = temp;
+            //        currentError = CalculateError(target, start);
+            //        Console.WriteLine("Mutated: " + start + " Error: " + currentError);
+            //        Delay.Restart();
+            //        while (Delay.ElapsedMilliseconds < 100) ;
+            //    }
+
+            //}
+
+            //Point[] points = new Point[]
+            //{
+            //    new Point(1,1),
+            //    new Point(0,0),
+            //    new Point(-3, -3),
+            //    new Point(-4, -3),
+            //    new Point(-4, -5),
+            //};
+
+            Point[] points = new Point[]
             {
-                string temp = MutateString(start);
-                if(currentError > CalculateError(target, temp))
-                {
-                    start = temp;
-                    currentError = CalculateError(target, start);
-                    Console.WriteLine("Mutated: " + start + " Error: " + currentError);
-                    Delay.Restart();
-                    while (Delay.ElapsedMilliseconds < 100) ;
-                }
+                new Point(3,3),
+                new Point(-3,3),
+                new Point(-10, -20),
+            };
 
-            }
+            var bestLine = FindBestLine(points);
         }
     }
 }
